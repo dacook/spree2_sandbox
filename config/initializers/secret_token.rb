2b56600b97bab1e36f775301849751f308dbbdd7
David Cook <david@redcliffs.net>
2013-07-19 16:17:20 +1000

    initial commit: spree 2.0.3 with spree_fancy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spree2Sandbox::Application.config.secret_token = '8b8093f53e8d20fff6fbf85a604b155b5eaeb9ab987bb21e71659138f4dc1cb99c68d57cbebb9895c4dd5c3f875a72ccfb064a4828f36d4f57a1687565b7acad'
